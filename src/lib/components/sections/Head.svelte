7e29d26930bb8126f9b3088a8e593a184a0f83e3
Corentin Gobbo <corentin.gobbo@hotmail.fr>
2023-11-27 10:12:53 +0100

    meta tags

--- src/lib/components/sections/Head.svelte
+++ src/lib/components/sections/Head.svelte
@@ -5,3 +5,34 @@
 
 <title>{title}</title>
 <meta name="description" content={description} />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+<link rel="manifest" href="/site.webmanifest" />
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+<meta name="msapplication-TileColor" content="#da532c" />
+<meta name="theme-color" content="#ffffff" />
+
+<meta
+	name="description"
+	content="Expertise du bâtiment - Solutions complètes pour vos problèmes de construction."
+/>
+<meta
+	name="keywords"
+	content="expertise, bâtiment, construction, sinistre, assurance, conseil technique"
+/>
+<meta name="author" content="Corentin Gobbo" />
+<meta name="robots" content="index, follow" />
+<meta name="language" content="French" />
+<meta name="og:title" content="8N - Expertise du bâtiment" />
+<meta name="og:description" content="Solutions complètes pour vos problèmes de construction." />
+<meta name="og:image" content="/hero-header.png" />
+<meta name="og:url" content="URL de votre site" />
+<meta name="og:type" content="website" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="8N - Expertise du bâtiment" />
+<meta
+	name="twitter:description"
+	content="Solutions complètes pour vos problèmes de construction."
+/>
+<meta name="twitter:image" content="/hero-header.png" />
